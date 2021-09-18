d9bb631b19dcabf4dc1d22b0c9de54f7e4f44f0f
Danie-cyber <danieljebarson.k2020@vitstudent.ac.in>
2021-09-18 20:03:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/PagerAdaptor.java
@@ -0,0 +1,45 @@
+package com.example.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdaptor extends FragmentPagerAdapter {
+
+    int tabcount;
+
+    public PagerAdaptor(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        tabcount=behavior;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position)
+        {
+            case 0:
+                return new chatfragment();
+
+            case 1:
+                return new statusfragment();
+
+            case 2:
+                return new callfragment();
+
+            default:
+                return null;
+
+        }
+
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return tabcount;
+    }
+}
